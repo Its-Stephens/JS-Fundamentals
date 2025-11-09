a73dfa14c69dca726224e8f4e7e744c87e67733a
DeWebSmith <okafors261@gmail.com>
2025-11-09 10:55:43 +0100

    Add conditional checks for arguments in 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+let i= 4;
+let p= 3;
+let g= 10;
+if (i< 4){
+    console.log("No argument")
+    i++;
+}
+else if(i< 4 || p<= 4){
+    console.log("Argument found")
+}
+else{
+    console.log("No argument")
+}
